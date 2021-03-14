f8d531061a97f35bdbcfe90de06f1709b2f038cd
Oleksii Patsiuk <patsyuck@ukr.net>
2021-03-14 20:18:46 +0200

    Виправив нєудобочітаємиє кускі

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import gallery from './gallery-items.js'
-/*console.log(gallery)*/
 const imagesNode = document.querySelector('ul.gallery')
+const modalNode = document.querySelector('div.lightbox')
+const imageNode = document.querySelector('img.lightbox__image')
+const closeNode = document.querySelector('button[data-action="close-lightbox"]')
+
 imagesNode.insertAdjacentHTML('beforeend', gallery.reduce((acc, item) => acc +
 `<li class="gallery__item">
     <a class="gallery__link" href="${item.original}">
@@ -8,19 +11,30 @@ imagesNode.insertAdjacentHTML('beforeend', gallery.reduce((acc, item) => acc +
     </a>
 </li>`, ''))
 
+function closeModalWindow() {
+    modalNode.classList.remove('is-open')
+    imageNode.setAttribute('src', '')
+}
+
 function openModalWindow(event) {
+
+
     event.preventDefault()
+
+
     const target = event.target
     /*console.log(target.getAttribute("data-source"))*/
-    const modalNode = document.querySelector('div.lightbox')
+    
+
     modalNode.classList.add('is-open')
-    const imageNode = document.querySelector('img.lightbox__image')
+    
+
     imageNode.setAttribute('src', target.getAttribute('data-source'))
-    const closeNode = document.querySelector('button[data-action="close-lightbox"]')
-    closeNode.addEventListener("click", () => {
-        modalNode.classList.remove('is-open')
-        imageNode.setAttribute('src', '')
-    })
+    
+
+    closeNode.addEventListener("click", closeModalWindow)
+
+    
 }
 
 imagesNode.addEventListener("click", openModalWindow)
